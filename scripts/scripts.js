5cecc83e5522e84644d249988cd48af45f60f940
brydave-EB <bryan.hunt@everbank.com>
2015-11-12 09:18:11 -0500

    adding check for blocks this is still not working consider adding a > 0 check

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,9 +11,21 @@ $(document).ready(function(){
 function createBlocks(){
   var $gridSize = $('input#gridnumber').val();
 
+  // first clear all of the blocks
+  $('.block').remove();
+
+  // then loop through the input number
+  // and generate that number of blocks
   for (var i = 0; i < $gridSize; i++){
     var $block = $('<div class="block"></div>');
     $('.grid').append($block);
+  }
+
+  // this is a test
+  if ($('.grid').has('.block')) {
+    console.log("It has a block");
+  } else {
+    console.log("nope it doesn't have blocks");
   }
 
   console.log($gridSize);
