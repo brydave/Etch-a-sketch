8141952895b1b5e510671aaeab046b0cc1d5cdad
brydave-EB <bryan.hunt@everbank.com>
2015-11-18 17:31:53 -0500

    adding basic styles This is for generic styling of blocks and spacing, nothing fancy

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,16 +17,11 @@ function createBlocks(){
   // then loop through the input number
   // and generate that number of blocks
   for (var i = 0; i < $gridSize; i++){
-    var $block = $('<div class="block"></div>');
+    var $block = $('<section class="block"></div>');
     $('.grid').append($block);
   }
 
-  // this is a test
-  if ($('.grid').has('.block')) {
-    console.log("It has a block");
-  } else {
-    console.log("nope it doesn't have blocks");
-  }
+  
 
   console.log($gridSize);
 }
