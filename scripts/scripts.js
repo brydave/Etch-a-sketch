6cc6903505d23fd3ddc914bb7397e846c2338a5a
Bryan <bryandhunt@gmail.com>
2015-11-19 08:02:19 -0500

    starting resize function I began by fixing some basic styles for responsive I also started to add in the resizeBlocks function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,26 +2,41 @@ $(document).ready(function(){
   console.log("This is ready to go!");
 
   $('#gridnumber').keypress(function(e){
-    if(e.keyCode==13)
+    if(e.keyCode==13){
       createBlocks();
+      resizeBlocks();
+    }
   });
 
-})
+});
 
 function createBlocks(){
   var $gridSize = $('input#gridnumber').val();
 
   // first clear all of the blocks
-  $('.block').remove();
+  $('.block').remove().fadeOut();
 
   // then loop through the input number
   // and generate that number of blocks
   for (var i = 0; i < $gridSize; i++){
     var $block = $('<section class="block"></div>');
-    $('.grid').append($block);
+    $('.grid').append($block).fadeIn();
   }
 
-  
-
   console.log($gridSize);
+}
+
+function resizeBlocks(){
+  // get grid area
+  // the area is the same for the number of blocks of a specific size
+  // consider decreasing the size by the number of blocks
+  // i.e. (500 blocks = specific area & block size) - block size/unit increase
+
+  // gathering up all of the quantities
+  var $area = $('.grid').width();
+  var $blockArea = $('.block').width();
+  var $blockNumber = $('input#gridnumber').val();
+
+  // determining size of block based on area
+
 }
